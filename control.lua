a0023014061e4802c9fd8bc8517ac303694d5b8d
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2018-02-28 00:57:00 +0900

    Show mini player view on the top-left corner

--- control.lua
+++ control.lua
@@ -1,41 +1,46 @@
 script.on_event(defines.events.on_player_created, function(event)
   local player = game.players[event.player_index]
+  create_camera_element(player)
 end)
 
 
 
 script.on_event(defines.events.on_tick, function(event)
-  if game.tick % 30 == 0 then
+  if game.tick % 1 == 0 then
     update_camera_element()
   end
 end)
 
 function create_camera_element(player)
-  player.print("create_camera_element")
-
   local root_element = player.gui.left
-  local camera_element = root_element.add {type = "camera", name="camera_element", position = player.position, surface_index = player.surface.index, zoom = 0.25}
-  camera_element.style.top_padding = 0
-  camera_element.style.bottom_padding = 0
-  camera_element.style.minimal_width = 2000
-  --camera_element.style.minimal_height = 270
+
+  local base_element = root_element.add {type = "frame", name="camera_base"}
+  base_element.style.top_padding = 8
+	base_element.style.left_padding = 8
+	base_element.style.right_padding = 8
+  base_element.style.bottom_padding = 8
+  base_element.style.title_top_padding = 0
+  base_element.style.title_right_padding = 0
+  base_element.style.title_bottom_padding = 2
+  base_element.style.title_left_padding = 0
+  base_element.style.maximal_width = 296
+
+  local camera_element = base_element.add {type = "camera", name="camera", position = player.position, surface_index = player.surface.index, zoom = 0.25}
+  camera_element.style.minimal_width = 280
+  camera_element.style.minimal_height = 280
+
+  local title_label = camera_element.add{type = "label", name = "title_label", caption = player.name}
+  title_label.style.top_padding = 0
+	title_label.style.left_padding = 8
 
   return camera_element
 end
 
 function update_camera_element()
   for _,player in pairs(game.players) do
-    player.print("update_camera_element")
+    local camera_element = player.gui.left.camera_base.camera
 
-    if player.controller_type ~= defines.controllers.ghost then
-      player.print("not ghost")
-      return
-    end
-    player.print("ghost")
-
-    local camera_element = player.gui.left.camera_element
-    if camera_element == nil then
-      camera_element = create_camera_element(player)
-    end
+    camera_element.position = player.position
+    camera_element.surface_index = player.surface.index
   end
 end
