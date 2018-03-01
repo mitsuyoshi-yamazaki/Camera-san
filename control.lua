369df55ec2797816a53b95498b25a5b6cd9512f9
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-03-01 15:58:47 +0900

    Fix new player's button not appearing issue

--- control.lua
+++ control.lua
@@ -1,8 +1,9 @@
-require "util"
+require "util"  -- I don't know what it does
 
 script.on_event(defines.events.on_player_created, function(event)
   local player = game.players[event.player_index]
   create_camera_element(player)
+  update_player_buttons()
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
@@ -49,7 +50,7 @@ end
 function create_camera_element(player)
   local root_element = player.gui.left
 
-  local base_element = root_element.add {type = "frame", name="camera_base", direction = "vertical"}
+  local base_element = root_element.add {type = "frame", name="camera_frame", direction = "vertical"}
   base_element.style.top_padding = 8
 	base_element.style.left_padding = 8
 	base_element.style.right_padding = 8
@@ -59,13 +60,6 @@ function create_camera_element(player)
   base_element.style.title_bottom_padding = 2
   base_element.style.title_left_padding = 0
   base_element.style.maximal_width = 296
-
-  for index,player in pairs(game.players) do
-    local button_name = get_button_name(player)
-    local button = base_element.add{type = "button", name = button_name, caption = player.name}
-    button.style.top_padding = 0
-	  button.style.left_padding = 8
-  end
 
   local camera_element = base_element.add {type = "camera", name="camera", position = player.position, surface_index = player.surface.index, zoom = 0.25}
   camera_element.style.minimal_width = 280
@@ -80,12 +74,54 @@ function create_camera_element(player)
   return camera_element
 end
 
+function update_player_buttons() 
+  remove_player_buttons()
+  add_player_buttons()
+end
+
+function remove_player_buttons()
+  local base_element = player.gui.left.camera_frame
+
+  local player_button_names = {}
+  for index,player in pairs(game.players) do
+    player_button_names[index] = get_button_name(player)
+  end
+
+  for _,player in pairs(game.players) do    
+    for child_element in base_element.children do
+      if has_value(player_button_names, child_element.name) then
+        child_element.destroy() -- Needs 'reverse' ?
+      end
+    end
+  end
+end
+
+function add_player_buttons()
+  local base_element = player.gui.left.camera_frame
+
+  for index,player in pairs(game.players) do
+    local button_name = get_button_name(player)
+    local button = base_element.add{type = "button", name = button_name, caption = player.name}
+    button.style.top_padding = 0
+	  button.style.left_padding = 8
+  end
+end
+
 function update_camera_element()
   for _,player in pairs(game.players) do
-    local camera_element = player.gui.left.camera_base.camera
+    local camera_element = player.gui.left.camera_frame.camera
     local target = get_target_for(player)
 
     camera_element.position = target.position
     camera_element.surface_index = target.surface.index
   end
+end
+
+local function has_value(table, value)
+  for _,v in pairs(table) do
+      if v == value then
+          return true
+      end
+  end
+  return false
 end
