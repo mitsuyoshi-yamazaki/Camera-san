3a515a40e04aee4c3d52088a0ce4354aa07fcf92
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2020-12-02 01:44:45 +0900

    Add zoom rate setting

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "camera-san-zoom",
+        setting_type = "runtime-per-user",
+        default_value = 0.25,
+        minimum_value = 0.1,
+        maximum_value = 2.0
+    }
+})
